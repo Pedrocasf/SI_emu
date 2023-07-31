51e3f8ae7109e0bb36b136b46ebceb9f6e90bb5a
Pedrocasf <pedrostarling2000@gmail.com>
2023-07-31 20:43:08 -0300

    Start SI emu proper

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use si::SI;
 use minifb::{Key, Window, WindowOptions};
 
 const WIDTH: usize = 256;
-const HEIGHT: usize = 224;
+const HEIGHT: usize = 256;
 
 fn main() {
+    #[cfg(feature = "log")]
+    simple_logger::init_with_level(log::Level::Error).unwrap();
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     let mut window = Window::new(
@@ -20,13 +22,30 @@ fn main() {
     });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16666)));
+    let mut s_i = SI::new();
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 0; // write something more funny here!
+        for i in 0..4167{
+            s_i.run_instr();
         }
-
+        for (i,px) in buffer.iter_mut().enumerate() {
+            if i == WIDTH * HEIGHT / 2{
+                break;
+            }
+            *px = if s_i.get_px(i as u16) {0xFFFFFFFF} else{0x00000000}
+        }
+        s_i.interrupt(0xCF);
+        
+        for i in 0..4167{
+            s_i.run_instr();
+        }
+        for (i,px) in buffer.iter_mut().enumerate().skip(WIDTH*HEIGHT/2) {
+            if i == WIDTH * HEIGHT{
+                break;
+            }
+            *px = if s_i.get_px(i as u16) {0xFFFFFFFF} else{0x00000000}
+        }
+        s_i.interrupt(0xD7);
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
             .update_with_buffer(&buffer, WIDTH, HEIGHT)
--- src/si.rs
+++ src/si.rs
@@ -1,26 +1,74 @@
 use crate::i8080_core;
-use i8080_core::CPU;
+use i8080_core::cpu::CPU;
+use log::error;
+use std::fs;
 pub struct SI{
     cpu:CPU,
-    pub mem:[u8;0x4000]
+    pub mem:[u8;0x4000],
+    shamt:u8,
+    shift_reg:u16,
+    watchdog:u8
 }
 impl SI{
+    const WATCHDOG_ADDDR:usize = 0x06;
+    const SHAMT_W_ADDR:usize = 0x02;
+    const SHIFT_REG_W_ADDR:usize = 0x04;
+    const SHIF_REG_R_ADDR:usize = 0x03;
     pub fn new()->SI{
         let mut si = SI{
             cpu:CPU::new(0,0),
-            mem:[0;0x4000]
+            mem:[0;0x4000],
+            shamt:0,
+            shift_reg:0,
+            watchdog:0xFF
+        };
+        let rom_h = fs::read("./roms/invaders.h").unwrap();
+        si.mem[0..0x800].copy_from_slice(&rom_h);
+        let rom_g = fs::read("./roms/invaders.g").unwrap();
+        si.mem[0x800..0x1000].copy_from_slice(&rom_g);
+        let rom_f = fs::read("./roms/invaders.f").unwrap();
+        si.mem[0x1000..0x1800].copy_from_slice(&rom_f);
+        let rom_e = fs::read("./roms/invaders.e").unwrap();
+        si.mem[0x1800..0x2000].copy_from_slice(&rom_e);
+        si
+    }
+    pub fn interrupt(&mut self, number:u8){
+        if self.cpu.interrupt_enabled{
+            let m = &mut self.mem;
+            self.cpu.instruction = number;
+            self.cpu.rst(m);
         }
-        let rom_h = fs::read("./roms/invaders.h");
-        si.mem[0..0x800].copy_from_slice(rom_h);
-        let rom_g = fs::read("./roms/invaders.g");
-        si.mem[0x800..0x1000].copy_from_slice(rom_g);
-        let rom_f = fs::read("./roms/invaders.f");
-        si.mem[0x1000..0x1800].copy_from_slice(rom_f);
-        let rom_e = fs::read("./roms/invaders.e");
-        si.mem[0x1800..0x2000].copy_from_slice(rom_e);
+    }
+    pub fn get_px(&mut self, coords:u16) -> bool{
+        (self.mem[0x2000+(coords>>3) as usize] >> (coords & 7)) == 1
     }
     pub fn run_instr(&mut self){
         let m = &mut self.mem;
+        if self.watchdog == 0{
+            self.cpu.instruction = 0xC7;
+        }
         self.cpu.next(m);
+        if self.cpu.out_strobe.0{
+            self.cpu.out_strobe.0 = false;
+            match self.cpu.out_strobe.1{
+                2 => self.shamt = self.cpu.out_strobe.2 & 7,
+                3 => {
+                    #[cfg(feature = "log")]
+                    error!("NO SOUND 3 ");
+                    //panic!("NO SOUND 3");
+                }
+                4 => self.shift_reg = ((self.cpu.out_strobe.2 as u16) << 8)| (self.shift_reg >> 8),
+                5 => {
+                    #[cfg(feature = "log")]
+                    error!("NO SOUND 5");
+                    //panic!("NO SOUND 5");
+                },
+                6 => {
+                    self.watchdog = self.cpu.out_strobe.2
+                },
+                _ => panic!("unimplemented IO {:02X}", self.cpu.out_strobe.1)
+            }
+        }
+        self.cpu.set_input_n(Self::SHIF_REG_R_ADDR as u8, (self.shift_reg >> (8-self.shamt)) as u8)
     }
 }
